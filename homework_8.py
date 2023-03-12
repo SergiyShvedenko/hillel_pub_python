e29030676feb24b7aaffa09e0670b15bb05762a6
SergiyShvedenko <127552339+SergiyShvedenko@users.noreply.github.com>
2023-03-12 19:17:09 +0000

    Add files via upload

---
+++ homework_8.py
@@ -0,0 +1,21 @@
+# Формирование списка:
+lst = []
+for i in range(200):
+    import random
+    random_number = random.randint(1, 10)
+    lst.append(random_number)
+print('Список рандом 200', lst)
+# Функция сортировки списка:
+def sort():
+    for el in lst:
+        if rez.get(el, None):
+            rez[el] += 1
+        else:
+            rez[el] = 1
+
+rez = {}
+sort()
+print('Словарь - результат', rez)
+
+rez_sorted = sorted(rez.items())
+print(dict(rez_sorted))
