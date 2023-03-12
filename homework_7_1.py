d91add6690a302725c442911b59786f430d38420
SergiyShvedenko <127552339+SergiyShvedenko@users.noreply.github.com>
2023-03-12 07:57:50 +0000

    Add files via upload

---
+++ homework_7_1.py
@@ -0,0 +1,24 @@
+
+def main():
+    for i in range(5):
+
+        valid = control()
+        if int(valid) == random_number:
+            print("Вітаємо, Вам в артилеристи!...  ")
+            break
+    else:
+        print('Жаль, попробуйте снова!')
+
+def control():
+    while True:
+        input_value = input('С пяти раз угадайте число от 0 до 10: ')
+        if input_value.isdigit() and int(input_value) >= 0 and int(input_value) <= 21:
+            break
+        print('Bam нужно внимательно вводить число от 0 до 21')
+    return input_value
+
+import random
+random_number = random.randint(1, 10)
+main()
+
+print('Верное число: ', random_number)
