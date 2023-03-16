e1b0311bc8a8ee5098b2d817a7841ab425eab456
SergiyShvedenko <127552339+SergiyShvedenko@users.noreply.github.com>
2023-03-16 00:10:38 +0000

    Update homework_10.py

--- homework_10.py
+++ homework_10.py
@@ -1,6 +1,6 @@
 
 values = [1, 2, '3', 'forth', 'end', 99, True, None]
 
-element = list(map(lambda x = int(x): str(x), values))
+element = list(map(lambda x: str(x), values))
 
 print(values, element)
