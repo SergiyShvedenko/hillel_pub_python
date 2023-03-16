d4876cba9126a4a0fb30b03a3b96fa3cad95993d
SergiyShvedenko <127552339+SergiyShvedenko@users.noreply.github.com>
2023-03-16 19:47:58 +0000

    Add files via upload

--- homework_10.py
+++ homework_10.py
@@ -1,6 +1,6 @@
 
 values = [1, 2, '3', 'forth', 'end', 99, True, None]
 
-element = list(map(lambda x: str(x), values))
+element = list(map(lambda x: str(x) if type(x) is int else x, values))
 
 print(values, element)
